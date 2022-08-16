37d5dca79edaabce0f60abf1188b08957f27929e
Azalia <azalia.pe.e>
2022-08-16 09:39:39 -0700

    Outlets

---
+++ Outlets/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Outlets
+//
+//  Created by Alumno on 8/16/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Un OUTLET es una conexión entre un Control de UI del Storyboard y una variable de Código Swift
+    @IBOutlet weak var lblMiEtiqueta: UILabel!
+    
+    //Esta función se ejecuta ya que cargó todo en memoria (antes de aparecer en pantalla).
+    //Lo que pongamos aquí sobreescribe lo del Storyboard
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblMiEtiqueta.text = "Este es texto desde código"
+        lblMiEtiqueta.textColor = UIColor.purple
+    }
+
+
+}
+
